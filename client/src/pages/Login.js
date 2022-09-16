d813b15cd6651c45e79058a893a9b8638c6c9597
Colin Scruggs <colinscruggs97@gmail.com>
2022-09-16 17:57:08 -0500

    Making it work again, lol

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -8,7 +8,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 const client_id = '16b80bb6a6604b0392a49028ddce5b60';
-const redirect_uri = 'http://localhost:3000';
+const redirect_uri = 'http://localhost:3000/home';
 const scope = 'user-read-private%20user-read-email%20user-read-recently-played%20user-top-read%20playlist-read-private'
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}&scope=${scope}`;
 
